7f251b45580cf62847514d2d40955f87588b063f
Carlos Luna <carlos.luna@nclouds.com>
2024-03-13 15:00:26 -0600

    Destroying secretsmanager, cloudformation and imagebuilder VPC fips endpoints

--- aws/vpc/endpoints.tf
+++ aws/vpc/endpoints.tf
@@ -26,25 +26,6 @@ resource "aws_vpc_endpoint" "kms" {
   }
 }
 
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id            = aws_vpc.main[0].id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  vpc_endpoint_type = "Interface"
-
-  private_dns_enabled = var.endpoint_private_dns_endpoints_enabled
-
-  security_group_ids = var.eks_node_groups_sg_id != "" ? [
-    aws_security_group.vpc_endpoints_eks["sm"].id,
-    aws_security_group.vpc_endpoints_https.id
-  ] : [aws_security_group.vpc_endpoints_https.id]
-
-  subnet_ids = var.name == "<account_name>" ? [for subnet in aws_subnet.tgw : subnet.id] : [for subnet in aws_subnet.aws_private : subnet.id]
-
-  tags = {
-    Name = "${var.name}-secretsmanager${var.fips_enabled == true ? "-fips" : ""}-endpoint"
-  }
-}
-
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id            = aws_vpc.main[0].id
   service_name      = "com.amazonaws.${data.aws_region.current.name}.ssm"
@@ -61,44 +42,6 @@ resource "aws_vpc_endpoint" "ssm" {
 
   tags = {
     Name = "${var.name}-ssm${var.fips_enabled == true ? "-fips" : ""}-endpoint"
-  }
-}
-
-resource "aws_vpc_endpoint" "imagebuilder" {
-  vpc_id            = aws_vpc.main[0].id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.imagebuilder"
-  vpc_endpoint_type = "Interface"
-
-  private_dns_enabled = var.endpoint_private_dns_endpoints_enabled
-
-  security_group_ids = var.eks_node_groups_sg_id != "" ? [
-    aws_security_group.vpc_endpoints_eks["imagebuilder"].id,
-    aws_security_group.vpc_endpoints_https.id
-  ] : [aws_security_group.vpc_endpoints_https.id]
-
-  subnet_ids = var.name == "<account_name>" ? [for subnet in aws_subnet.tgw : subnet.id] : [for subnet in aws_subnet.aws_private : subnet.id]
-
-  tags = {
-    Name = "${var.name}-imagebuilder${var.fips_enabled == true ? "-fips" : ""}-endpoint"
-  }
-}
-
-resource "aws_vpc_endpoint" "cloudformation" {
-  vpc_id            = aws_vpc.main[0].id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.cloudformation"
-  vpc_endpoint_type = "Interface"
-
-  private_dns_enabled = var.endpoint_private_dns_endpoints_enabled
-
-  security_group_ids = var.eks_node_groups_sg_id != "" ? [
-    aws_security_group.vpc_endpoints_eks["cloudformation"].id,
-    aws_security_group.vpc_endpoints_https.id
-  ] : [aws_security_group.vpc_endpoints_https.id]
-
-  subnet_ids = var.name == "<account_name>" ? [for subnet in aws_subnet.tgw : subnet.id] : [for subnet in aws_subnet.aws_private : subnet.id]
-
-  tags = {
-    Name = "${var.name}-cloudformation${var.fips_enabled == true ? "-fips" : ""}-endpoint"
   }
 }
 
