5f3418b5a6803f1a51ee3173e72f2d081d91aaf3
Carlos Luna <carlos.luna@nclouds.com>
2024-03-15 10:38:34 -0600

    Add DMZ routes for IGW INGRESS RT

--- aws/vpc/locals.tf
+++ aws/vpc/locals.tf
@@ -3,5 +3,5 @@ locals {
     for i in aws_networkfirewall_firewall.firewall[0].firewall_status[0].sync_states : i.availability_zone => i.attachment[0].endpoint_id
     if var.enable_firewall
   }, {})
-  protected_pub_cidr_blocks = { for i in aws_subnet.public : i.availability_zone => i.cidr_block }
+  protected_pub_cidr_blocks = { for i in merge(aws_subnet.public_dmz, aws_subnet.public) : i.availability_zone => i.cidr_block }
 }
