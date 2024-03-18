adb6d0f0cbce03e64e093186647bfba2aee8773e
Carlos Luna <carlos.luna@nclouds.com>
2024-03-18 08:18:18 -0600

    Add data blocks that are missing

--- aws/transit-gateway/data.tf
+++ aws/transit-gateway/data.tf
@@ -1,1 +1,10 @@
+data "aws_region" "current" {}
+
+data "aws_arn" "logs_bucket" {
+  arn = var.flow_logs_bucket
+}
+
+data "aws_availability_zones" "this" {}
+
+
 data "aws_caller_identity" "current" {}
