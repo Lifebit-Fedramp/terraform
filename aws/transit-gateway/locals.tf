a99eab9a69905248eb9dca48dae98f8a08773e97
Carlos Luna <carlos.luna@nclouds.com>
2024-03-21 12:14:53 -0600

    fix consistenty types for tgw_accepter_ids

--- aws/transit-gateway/locals.tf
+++ aws/transit-gateway/locals.tf
@@ -3,5 +3,5 @@ locals {
     for account in var.accounts_to_share : account if account != data.aws_caller_identity.current.account_id
   ]
 
-  tgw_accepter_ids = var.accept_tgw_attachment ? tomap(var.vpc_attachment_id_map) : []
+  tgw_accepter_ids = var.accept_tgw_attachment ? var.vpc_attachment_id_map : {}
 }
