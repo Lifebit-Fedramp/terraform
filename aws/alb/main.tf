fb2ac1dd32502ad3457add19bcff54a3d93d5ce3
trumpetgrace <33424366+trumpetgrace@users.noreply.github.com>
2024-04-25 14:26:33 -0700

    Update alb module, add outputs and readme

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -1,42 +1,78 @@
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "9.9.0"
+  create  = var.create
 
-  name = var.name
-
+  name               = var.name
   load_balancer_type = "application"
+  vpc_id             = var.vpc_id
+  subnets            = var.subnets
 
-  vpc_id          = var.vpc_id
-  subnets         = var.subnets
-  security_groups = var.security_groups
-
-  target_groups = [
-    {
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets          = {}
+  listeners = {
+    http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
-  ]
+    https = {
+      port     = 443
+      protocol = "HTTPS"
+      #      certificate_arn = var.certificate_arn
 
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = var.certificate_arn
-      target_group_index = 0
+      fixed_response = {
+        content_type = "text/plain"
+        message_body = "Page not found"
+        status_code  = 200
+      }
     }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Key = "alb-fips-enabled"
   }
+
+  security_group_name            = "${var.name}-alb-sg"
+  security_group_use_name_prefix = var.security_group_use_name_prefix
+  security_group_description     = format("Security group for ALB %s", var.name)
+  security_group_ingress_rules = var.private_ingress_sg_rules == {} ? {
+    all_https = {
+      from_port = 443
+      to_port   = 443
+      protocol  = "tcp"
+      cidr_ipv4 = "0.0.0.0/0"
+    }
+    all_http = {
+      from_port = 80
+      to_port   = 80
+      protocol  = "tcp"
+      cidr_ipv4 = "0.0.0.0/0"
+    }
+  } : var.private_ingress_sg_rules
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  access_logs                                 = var.access_logs
+  connection_logs                             = var.connection_logs
+  client_keep_alive                           = var.client_keep_alive
+  customer_owned_ipv4_pool                    = var.customer_owned_ipv4_pool
+  desync_mitigation_mode                      = var.desync_mitigation_mode # defensive is default - do we want strictest? https://docs.aws.amazon.com/securityhub/latest/userguide/elb-controls.html#elb-12
+  drop_invalid_header_fields                  = var.drop_invalid_header_fields
+  enable_cross_zone_load_balancing            = var.enable_cross_zone_load_balancing
+  enable_deletion_protection                  = var.enable_deletion_protection
+  enable_tls_version_and_cipher_suite_headers = var.enable_tls_version_and_cipher_suite_headers
+  enable_xff_client_port                      = var.enable_xff_client_port
+  idle_timeout                                = var.idle_timeout
+  internal                                    = var.internal
+  preserve_host_header                        = var.preserve_host_header
+  xff_header_processing_mode                  = var.xff_header_processing_mode
+  timeouts                                    = var.timeouts
+  security_group_tags                         = var.security_group_tags
+
+  tags = merge(var.tags, {
+    Key = "alb-fips-enabled"
+  })
 }
--- aws/alb/variables.tf
+++ aws/alb/variables.tf
@@ -1,5 +1,191 @@
-variable "vpc_id" {}
-variable "subnets" {}
-variable "certificate_arn" {}
-variable "name" {}
-variable "security_groups" {}
+variable "create" {
+  description = "Controls if resources should be created (affects nearly all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+################################################################################
+# Load Balancer
+################################################################################
+
+variable "access_logs" {
+  description = "Map containing access logging configuration for load balancer"
+  type        = map(string)
+  default     = {}
+}
+
+variable "connection_logs" {
+  description = "Map containing access logging configuration for load balancer"
+  type        = map(string)
+  default     = {}
+}
+
+variable "client_keep_alive" {
+  description = "Client keep alive value in seconds. The valid range is 60-604800 seconds. The default is 3600 seconds."
+  type        = number
+  default     = null
+}
+
+variable "customer_owned_ipv4_pool" {
+  description = "The ID of the customer owned ipv4 pool to use for this load balancer"
+  type        = string
+  default     = null
+}
+
+variable "desync_mitigation_mode" {
+  description = "Determines how the load balancer handles requests that might pose a security risk to an application due to HTTP desync. Valid values are `monitor`, `defensive` (default), `strictest`"
+  type        = string
+  default     = null
+}
+
+variable "drop_invalid_header_fields" {
+  description = "Indicates whether HTTP headers with header fields that are not valid are removed by the load balancer (`true`) or routed to targets (`false`). The default is `true`. Elastic Load Balancing requires that message header names contain only alphanumeric characters and hyphens. Only valid for Load Balancers of type `application`"
+  type        = bool
+  default     = true
+}
+
+variable "enable_cross_zone_load_balancing" {
+  description = "If `true`, cross-zone load balancing of the load balancer will be enabled. For application load balancer this feature is always enabled (`true`) and cannot be disabled. Defaults to `true`"
+  type        = bool
+  default     = true
+}
+
+variable "enable_deletion_protection" {
+  description = "If `true`, deletion of the load balancer will be disabled via the AWS API. This will prevent Terraform from deleting the load balancer. Defaults to `true`"
+  type        = bool
+  default     = true
+}
+
+variable "enable_tls_version_and_cipher_suite_headers" {
+  description = "Indicates whether the two headers (`x-amzn-tls-version` and `x-amzn-tls-cipher-suite`), which contain information about the negotiated TLS version and cipher suite, are added to the client request before sending it to the target. Only valid for Load Balancers of type `application`. Defaults to `false`"
+  type        = bool
+  default     = null
+}
+
+variable "enable_xff_client_port" {
+  description = "Indicates whether the X-Forwarded-For header should preserve the source port that the client used to connect to the load balancer in `application` load balancers. Defaults to `false`"
+  type        = bool
+  default     = null
+}
+
+variable "idle_timeout" {
+  description = "The time in seconds that the connection is allowed to be idle. Only valid for Load Balancers of type `application`. Default: `60`"
+  type        = number
+  default     = null
+}
+
+variable "internal" {
+  description = "If true, the LB will be internal. Defaults to `false`"
+  type        = bool
+  default     = null
+}
+
+variable "name" {
+  description = "The name of the LB. This name must be unique within your AWS account, can have a maximum of 32 characters, must contain only alphanumeric characters or hyphens, and must not begin or end with a hyphen"
+  type        = string
+  default     = null
+}
+
+variable "name_prefix" {
+  description = "Creates a unique name beginning with the specified prefix. Conflicts with `name`"
+  type        = string
+  default     = null
+}
+
+variable "preserve_host_header" {
+  description = "Indicates whether the Application Load Balancer should preserve the Host header in the HTTP request and send it to the target without any change. Defaults to `false`"
+  type        = bool
+  default     = null
+}
+
+variable "subnets" {
+  description = "A list of subnet IDs to attach to the LB. Subnets cannot be updated for Load Balancers of type `network`. Changing this value for load balancers of type `network` will force a recreation of the resource"
+  type        = list(string)
+  default     = null
+}
+
+variable "xff_header_processing_mode" {
+  description = "Determines how the load balancer modifies the X-Forwarded-For header in the HTTP request before sending the request to the target. The possible values are `append`, `preserve`, and `remove`. Only valid for Load Balancers of type `application`. The default is `append`"
+  type        = string
+  default     = null
+}
+
+variable "timeouts" {
+  description = "Create, update, and delete timeout configurations for the load balancer"
+  type        = map(string)
+  default     = {}
+}
+
+################################################################################
+# Target Group
+################################################################################
+
+variable "target_groups" {
+  description = "Map of target group configurations to create"
+  type        = any
+  default     = {}
+}
+
+################################################################################
+# Security Group
+################################################################################
+
+variable "security_group_name" {
+  description = "Name to use on security group created"
+  type        = string
+  default     = null
+}
+
+variable "security_group_use_name_prefix" {
+  description = "Determines whether the security group name (`security_group_name`) is used as a prefix"
+  type        = bool
+  default     = true
+}
+
+variable "security_group_description" {
+  description = "Description of the security group created"
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "Identifier of the VPC where the security group will be created"
+  type        = string
+  default     = null
+}
+
+variable "security_group_ingress_rules" {
+  description = "Security group ingress rules to add to the security group created"
+  type        = any
+  default     = {}
+}
+
+variable "security_group_egress_rules" {
+  description = "Security group egress rules to add to the security group created"
+  type        = any
+  default     = {}
+}
+
+variable "security_group_tags" {
+  description = "A map of additional tags to add to the security group created"
+  type        = map(string)
+  default     = {}
+}
+
+variable "certificate_arn" {
+  description = "Certificate ARN to associate with ALB"
+  type        = string
+  default     = ""
+}
+
+variable "private_ingress_sg_rules" {
+  description = "Map of private ingress security group rules"
+  type        = map(any)
+  default     = {}
+}
