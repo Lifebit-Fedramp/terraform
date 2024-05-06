523d527f0f080b949272b260fe307c2def178077
jorge-nclouds <jorge@nclouds.com>
2024-05-06 13:13:32 -0600

    update s3 module

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -3,20 +3,30 @@ module "s3" {
   version = "3.14.0"
 
   bucket        = var.bucket
-  force_destroy = false
+  force_destroy = var.force_destroy
 
+  acceleration_status                   = var.acceleration_status
   attach_policy                         = var.attach_policy
   policy                                = var.attach_policy ? var.policy : null
   attach_deny_insecure_transport_policy = true
   attach_require_latest_tls_policy      = true
 
-  acl                     = var.acl
-  block_public_acls       = var.block_public_acls
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  acl                      = var.acl
+  block_public_acls        = var.block_public_acls
+  block_public_policy      = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
+  request_payer            = var.request_payer
+  cors_rule                = var.cors_rule
+  attach_public_policy     = var.attach_public_policy
+  control_object_ownership = var.control_object_ownership
+  intelligent_tiering      = var.intelligent_tiering
+  object_lock_enabled      = var.object_lock_enabled
+  object_ownership         = var.object_ownership
+  lifecycle_rule           = var.lifecycle_rule
+  logging                  = var.logging
 
-  replication_configuration = var.replication_configuration
+  replication_configuration            = var.replication_configuration
   server_side_encryption_configuration = var.server_side_encryption_configuration
 
   versioning = {
--- aws/s3/variables.tf
+++ aws/s3/variables.tf
@@ -1,3 +1,9 @@
+variable "acceleration_status" {
+  description = "(Optional) Sets the accelerate configuration of an existing bucket. Can be Enabled or Suspended."
+  type        = string
+  default     = null
+}
+
 variable "acl" {
   description = "The canned ACL to apply."
   type        = string
@@ -8,6 +14,12 @@ variable "attach_policy" {
   description = "Should an iam policy should be attached to the bucket?"
   type        = bool
   default     = false
+}
+
+variable "attach_public_policy" {
+  description = "Controls if a user defined public bucket policy will be attached (set to `false` to allow upstream to apply defaults to the bucket)"
+  type        = bool
+  default     = true
 }
 
 variable "block_public_acls" {
@@ -22,6 +34,60 @@ variable "bucket" {
   default     = ""
 }
 
+variable "control_object_ownership" {
+  description = "Whether to manage S3 Bucket Ownership Controls on this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "cors_rule" {
+  description = "List of maps containing rules for Cross-Origin Resource Sharing."
+  type        = any
+  default     = []
+}
+
+variable "force_destroy" {
+  description = "(Optional, Default:false ) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  type        = bool
+  default     = false
+}
+
+variable "intelligent_tiering" {
+  description = "Map containing intelligent tiering configuration."
+  type        = any
+  default     = {}
+}
+
+variable "kms_key_arn" {
+  description = "(Optional) The ARN for the KMS encryption key. When specified, the bucket will use the specified encryption key. Otherwise, the bucket will use the default AWS managed key (SSE-S3)."
+  type        = string
+  default     = null
+}
+
+variable "lifecycle_rule" {
+  description = "List of maps containing configuration of object lifecycle management."
+  type        = any
+  default     = []
+}
+
+variable "logging" {
+  description = "Map containing versioning configuration."
+  type        = any
+  default     = {}
+}
+
+variable "object_lock_enabled" {
+  description = "Whether S3 bucket should have an Object Lock configuration enabled."
+  type        = bool
+  default     = false
+}
+
+variable "object_ownership" {
+  description = "Object ownership. Valid values: BucketOwnerEnforced, BucketOwnerPreferred or ObjectWriter. 'BucketOwnerEnforced': ACLs are disabled, and the bucket owner automatically owns and has full control over every object in the bucket. 'BucketOwnerPreferred': Objects uploaded to the bucket change ownership to the bucket owner if the objects are uploaded with the bucket-owner-full-control canned ACL. 'ObjectWriter': The uploading account will own the object if the object is uploaded with the bucket-owner-full-control canned ACL."
+  type        = string
+  default     = "BucketOwnerEnforced"
+}
+
 variable "policy" {
   description = "JSON policy attached to the bucket"
   type        = string
@@ -32,6 +98,12 @@ variable "replication_configuration" {
   description = "Map containing cross-region replication configuration."
   type        = any
   default     = {}
+}
+
+variable "request_payer" {
+  description = "(Optional) Specifies who should bear the cost of Amazon S3 data transfer. Can be either BucketOwner or Requester. By default, the owner of the S3 bucket would incur the costs of any data transfer. See Requester Pays Buckets developer guide for more information."
+  type        = string
+  default     = null
 }
 
 variable "server_side_encryption_configuration" {
