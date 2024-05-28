b0bcdcfda8deac2dfa6954d85714818233332e5c
trumpetgrace <33424366+trumpetgrace@users.noreply.github.com>
2024-05-28 15:06:22 -0700

    Set default placeholder value for secret strings

--- aws/ssm-parameter/main.tf
+++ aws/ssm-parameter/main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameter" {
   name        = "${var.path_prefix}/${each.key}"
   description = each.value.description
   type        = each.value.value != "" ? "String" : "SecureString"
-  value       = each.value.value
+  value       = each.value.value != "" ? each.value.value : "<placeholder>"
   key_id      = each.value.value != "" ? null : var.key_id
 
   tags = var.tags
