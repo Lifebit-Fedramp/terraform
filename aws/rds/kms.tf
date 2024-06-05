7080481224c896ab1562f0e73b95d4966bd39e14
trumpetgrace <33424366+trumpetgrace@users.noreply.github.com>
2024-06-05 15:41:03 -0700

    Update rds kms key policy

--- aws/rds/kms.tf
+++ aws/rds/kms.tf
@@ -1,75 +1,36 @@
 resource "aws_kms_key" "rds_kms_cmk" {
   deletion_window_in_days = 30
   enable_key_rotation     = true
-  policy                  = data.aws_iam_policy_document.rds_kms_cmk_policy.json
+  policy                  = data.aws_iam_policy_document.rds_key_policy.json
 }
 
-# kics ignore-block
-data "aws_iam_policy_document" "rds_kms_cmk_policy" {
+data "aws_iam_policy_document" "rds_key_policy" {
   statement {
-    effect = "Allow"
-    principals {
-      identifiers = [
-        "logs.${data.aws_region.current.name}.amazonaws.com"
-      ]
-      type = "Service"
-    }
-    actions = [
-      "kms:CreateKey",
-      "kms:CreateGrant",
-      "kms:Decrypt",
-      "kms:Describe*",
-      "kms:Get*",
-      "kms:Encrypt",
-      "kms:GenerateDataKey*",
-      "kms:ListGrants",
-      "kms:ListKey",
-      "kms:PutKey",
-      "kms:ReEncrypt*",
-      "kms:RevokeGrant"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    principals {
-      identifiers = [
-        "arn:aws-us-gov:iam::${data.aws_caller_identity.current.account_id}:root"
-      ]
-      type = "AWS"
-    }
+    sid       = "Enable IAM User Permissions"
     actions   = ["kms:*"]
     resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws-us-gov:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
   }
 
   statement {
-    effect = "Allow"
-    principals {
-      identifiers = ["*"]
-      type        = "AWS"
-    }
     actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
+      "kms:Encrypt*",
+      "kms:Decrypt*",
       "kms:ReEncrypt*",
       "kms:GenerateDataKey*",
-      "kms:CreateGrant",
-      "kms:listGrants",
-      "kms:DescribeKey"
+      "kms:Describe*"
     ]
     resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      values   = ["rds.${data.aws_region.current.name}.amazonaws.com"]
-      variable = "kms:ViaService"
-    }
-    condition {
-      test     = "StringEquals"
-      values   = ["${data.aws_caller_identity.current.account_id}"]
-      variable = "kms:CallerAccount"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "rds.us-gov-west-1.amazonaws.com"
+      ]
     }
   }
 }
