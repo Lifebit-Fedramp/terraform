4d8151506394810ff22bd4c7a46f7ba4aff52447
Mark Mastoras <mark.mastoras@foghornconsulting.com>
2024-06-10 14:31:30 -0700

    wip

--- aws/tenable-infra/main.tf
+++ aws/tenable-infra/main.tf
@@ -156,7 +156,5 @@ module "ec2_instance" {
   user_data_replace_on_change = false
   vpc_security_group_ids      = var.vpc_security_group_ids
 
-  root_block_device = {
-    volume_size = 60
-  }
+  root_block_device           = 60
 }
